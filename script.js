67ebfc0c29a6f9d3dbe7c12e129fd6a2a0e13ec9
Adam <adammiller.2000nj@gmail.com>
2024-01-02 15:57:08 -0500

    fixed definition change bug

--- script.js
+++ script.js
@@ -1,50 +1,46 @@
 const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
-const result = document.querySelector('.result');
 const sound = document.getElementById('sound');
 const soundBtn = document.querySelector('.btn-group > button:nth-child(1)');
 const searchBtn = document.getElementById('searchBtn');
 const changeBtn = document.querySelector('.btn-group > button:nth-child(2)');
 let input = document.getElementById('input');
-let startNum = 0;
 
 searchBtn.addEventListener('click', () => {
-    startNum = 0;
+    changeBtn.style.display = 'inline-block'
+    let startNum = 0;
     fetch(url + input.value).then((response) => response.json())
     .then((data) => {
-        console.log(data)
-        document.querySelector('.word > h3').innerHTML = data[0].word;
-        document.querySelector('.detail p:nth-child(1)').innerHTML = data[0].meanings[startNum].partOfSpeech;
-        document.querySelector('.detail p:nth-child(2)').innerHTML = data[0].phonetics[1].text;
-        document.querySelector('.definition').innerHTML = data[0].meanings[startNum].definitions[0].definition;
-        document.querySelector('.example').innerHTML = data[0].meanings[startNum].definitions[0].example;
-        sound.src = data[0].phonetics[startNum].audio;
-        input.value = '';
+        console.log(data) // DATA IN CONSOLE
+        fillData();
 
         changeBtn.addEventListener('click', () => {
             if (startNum >= data[0].meanings.length - 1){
                 startNum = 0;
-            } else {
-                startNum++;
             };
+            startNum++;
+            fillData();
+        })
+
+        function fillData() {
+            document.querySelector('.word > h3').innerHTML = data[0].word;
+            document.querySelector('.detail p:nth-child(1)').innerHTML = data[0].meanings[startNum].partOfSpeech;
+            document.querySelector('.detail p:nth-child(2)').innerHTML = data[0].phonetics[1].text;
             document.querySelector('.definition').innerHTML = data[0].meanings[startNum].definitions[0].definition;
             document.querySelector('.example').innerHTML = data[0].meanings[startNum].definitions[0].example;
-            document.querySelector('.detail p:nth-child(1)').innerHTML = data[0].meanings[startNum].partOfSpeech;
-        })
+            sound.src = data[0].phonetics[startNum].audio;
+            input.value = '';
+        }
     })
     .catch( () => {
         document.querySelector('.word > h3').innerHTML = 'ERROR';
-        document.querySelector('.detail p:nth-child(1)').innerHTML = '';
-        document.querySelector('.detail p:nth-child(2)').innerHTML = '';
-        document.querySelector('.definition').innerHTML = '';
-        document.querySelector('.example').innerHTML = '';
+        document.querySelectorAll('p').forEach((el) => el.innerHTML = '');
         sound.src = '';
-        changeBtn.disabled = true;
+        changeBtn.style.display = 'none';
     })
 })
 
-input.addEventListener('keydown', function(e) {
+window.addEventListener('keydown', function(e) {
     if(e.key === 'Enter'){
-        e.preventDefault;
         searchBtn.click();
     }
 })
